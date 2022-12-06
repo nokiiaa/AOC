5d7f43e2d6c07304aeedebc3e0f489c65d3d5ff0
npad <@>
2022-12-06 14:55:28 +0300

    Shortened day 6 again

--- y2022/day6.py
+++ y2022/day6.py
@@ -1,2 +1,2 @@
 def day6(data):
-  return ([i+n for i in range(len(data)-n) if len(set(data[i:i+n])) == n][0] for n in (4, 14))
+  return ([i for i in range(n,len(data)) if len(set(data[i-n:i])) == n][0] for n in (4, 14))
