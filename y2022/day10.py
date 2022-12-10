5934978fbb7da250575eda42012ee608528b78e8
npad <@>
2022-12-10 13:58:40 +0300

    Shortened days 9 and 10

--- y2022/day10.py
+++ y2022/day10.py
@@ -7,18 +7,14 @@ def day10(data):
     D, M = divmod(c - 1, 40)
     if c < 40*6 + 1:
       print('#' if x-1 <= M <= x+1 else '.', end=('\n' if M==39 else ''))
-    if m == 0 and 0 <= d <= 5:
-      return c * x
-    return 0
+    return c * x if m == 0 and 0 <= d <= 5 else 0
   check(cycles)
   for l in data.splitlines():
     try:
-      inc = int(re.findall('-*\d+', l)[0])
-      ans += check(cycles + 1)
-      x += inc
-      ans += check(cycles + 2)
-      cycles += 2
+      for i in range(2):
+        cycles += 1
+        ans += check(cycles)
+        x += int(re.findall('-*\d+', l)[i])
     except:
-      cycles += 1
-      ans += check(cycles)
+      pass
   return ans, ans
--- y2022/day9.py
+++ y2022/day9.py
@@ -1,19 +1,19 @@
+from operator import add, sub
 from math import trunc
 
 def day9(data):
   def simulate(n):
-    positions = [(0, 0)]
+    positions = []
     table = {'L': (-1,0), 'R': (1,0), 'U': (0,1), 'D': (0,-1)}
     knots = [(0, 0) for i in range(n)]
     for op in data.splitlines():
       d, o = op.split(' ')
-      dx, dy = table[d]
       for i in range(int(o)):
-        knots[n-1] = (knots[n-1][0] + dx, knots[n-1][1] + dy)
+        knots[n-1] = tuple(map(add, knots[n-1], table[d]))
         for j in reversed(range(1, n)):
-          diff = (knots[j][0] - knots[j-1][0], knots[j][1] - knots[j-1][1])
-          if diff[0] ** 2 + diff[1] ** 2 > 2:
-            knots[j-1] = (knots[j][0] - trunc(diff[0]/2), knots[j][1] - trunc(diff[1]/2))
+          diff = tuple(map(sub, knots[j], knots[j-1]))
+          if sum(c**2 for c in diff) > 2:
+            knots[j-1] = tuple(map(sub, knots[j], [trunc(c/2) for c in diff]))
             if j == 1 and knots[0] not in positions:
               positions.append(knots[0])
     return len(positions)
